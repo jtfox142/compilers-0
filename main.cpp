3930155cf8622104f319d348e38b0a25fe65ce07
Jacob (J.T.) Fox <jtfox142@gmail.com>
2024-01-23 15:34:48 -0600

    changed input handling to be in spec

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include "inputParsing.hpp"
-
 #include <iostream>
 #include <vector>
 #include <string_view>
+#include <string>
+#include <stdexcept>
+#include <fstream>
 //#include "node.h"
 //#include "tree.h"
 
@@ -10,8 +11,33 @@ int main(int argc, char* argv[]) {
     // process command line arguments first and error if improper
     // if filename given, make sure file is readable, error otherwise
     // set up keyboard processing so that below this point there is only one version of the code
+    std::vector<std::string_view> preTreeInput;
 
-    inputParsing::parse(argc, argv);
+    try {
+        //If there are no provided args, initiate place user input into a temporary file
+        if(argc <= 1) {
+            static std::ofstream defaultFile;
+            defaultFile.open("default.txt", std::ios::out);
+
+            std::string userInput;
+            std::cout << "Please enter your input: ";
+            std::cin >> userInput;
+
+            defaultFile << userInput;
+            defaultFile.close();
+
+            std::cout << "\nDefault file has received input.\n";
+
+            readFromFile("default.txt", &preTreeInput);
+        }
+        else {
+            std::string fileName = argv[1];
+            readFromFile(fileName, &preTreeInput);
+        }
+    } catch(const std::exception& ex) {
+        std::cerr << ex.what() << '\n';
+    }
+    
 
     for(const auto& arg : args) {
         node::node_t *root = tree::buildTree(file);
@@ -22,4 +48,24 @@ int main(int argc, char* argv[]) {
     tree::printPostorder(root);
 
     return 0;
+}
+
+void readFromFile(std::string_view filename, std::vector<std::string_view>* input) {
+    static std::ifstream inputFile (filename);
+    std::string word;
+
+    if(inputFile.is_open()) {
+        while(!inputFile.eof()) {
+            //Not sure I need to do this every time, but the files should be small enough that the extra overhead will not matter
+            if(inputFile.bad() || inputFile.fail()) {
+                throw std::runtime_error("ERROR: bad file read");
+            }
+
+            inputFile >> word;
+            input->push_back(word);
+        }
+    }
+    else {
+        throw std::runtime_error("ERROR: could not open file");
+    }
 }
